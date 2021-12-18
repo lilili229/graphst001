6f7cc78cd2d64d2d5a13ff8b00e856481e40ff16
lilili229 <96349174+lilili229@users.noreply.github.com>
2021-12-19 00:14:47 +0700

    Add files via upload

---
+++ graph.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System;
+
+namespace Do_An_Graph
+{
+    public class Distoriginal
+    {
+        public int distance; public int parentVert;
+        public Distoriginal(int pv, int d)
+        {
+            distance = d; parentVert = pv;
+        }
+    }
+    public class Vertex
+    {
+        public Location label;
+        public bool isInTree;
+        public Vertex(Location lab) { label = lab; isInTree = false; }
+    }
+    public class Graph
+    {
+        private const int max_verts = 10;
+        int infinity = 100;
+        Vertex[] vertexList; int[,] adjMat; int[,] adjMat2;
+        int nVerts; int nTree; Distoriginal[] spath;
+        int currentVert; int startToCurrent;
+
+        public Graph()
+        {
+            vertexList = new Vertex[max_verts];
+            adjMat = new int[max_verts, max_verts];
+            adjMat2 = new int[max_verts, max_verts];
+            nVerts = 0; nTree = 0;
+            for (int j = 0; j < max_verts; j++)
+                for (int k = 0; k < max_verts; k++)
+                    adjMat[j, k] = infinity;
+            spath = new Distoriginal[max_verts];
+        }
+
+        public int Property
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void AddVertex(Location lab)
+        {
+            vertexList[nVerts] = new Vertex(lab);
+            nVerts++;
+        }
+        public void AddEdge(int start, int theEnd, int weight)
+        {
+            adjMat[start, theEnd] = weight;
+            adjMat2[start, theEnd] = 1;
+        }
+    /*    public void Path()
+        {
+            int startTree = 0;
+            vertexList[startTree].isInTree = true;
+            nTree = 1;
+            for (int j = 0; j < nVerts; j++)
+            {
+                int tempDist = adjMat[startTree, j];
+                spath[j] = new Distoriginal(startTree, tempDist);
+            }
+            while (nTree < nVerts)
+            {
+                int indexMin = GetMin();
+                int minDist = spath[indexMin].distance;
+                currentVert = indexMin;
+                startToCurrent = spath[indexMin].distance;
+                vertexList[currentVert].isInTree = true;
+                nTree++;
+                AdjustShortPath();
+            }
+            int sc = 0;
+            DisplayPaths(sc);
+            nTree = 0;
+            for (int j = 0; j < nVerts; j++)
+                vertexList[j].isInTree = false;
+        }*/
+        public int GetMin()
+        {
+            int minDist = infinity;
+            int indexMin = 0;
+            for (int j = 1; j <= nVerts - 1; j++)
+                if (!(vertexList[j].isInTree) && spath[j].distance < minDist)
+                {
+                    minDist = spath[j].distance; indexMin = j;
+                }
+            return indexMin;
+        }
+        public void AdjustShortPath()
+        {
+            int column = 1;
+            while (column < nVerts)
+                if (vertexList[column].isInTree) column++;
+                else
+                {
+                    int currentToFring = adjMat[currentVert, column];
+                    int startToFringe = startToCurrent + currentToFring;
+                    int sPathDist = spath[column].distance;
+                    if (startToFringe < sPathDist)
+                    {
+                        spath[column].parentVert = currentVert;
+                        spath[column].distance = startToFringe;
+                    }
+                    column++;
+                }
+        }
+        public void DisplayPaths(int nVerts)
+        {
+            for (int j = 0; j <= nVerts - 1; j++)
+            {
+                Location parent = vertexList[spath[0].parentVert].label;
+                Console.Write("Từ " + parent.GetName() + " đến " + vertexList[j].label.GetName() + " = ");
+                if (spath[j].distance == infinity) Console.Write("inf");
+                    else Console.Write(spath[j].distance + " km");
+            }
+        }
+
+        public void DisplayPaths2(string cs)
+        {
+            
+            switch (cs)
+            {
+                case "A": DisplayPaths(0); break;
+                case "B": DisplayPaths(1); break;
+                case "C": DisplayPaths(2); break;
+                case "D": DisplayPaths(3); break;
+                case "E": DisplayPaths(4); break;
+                case "H": DisplayPaths(5); break;
+                case "I": DisplayPaths(6); break;
+                case "V": DisplayPaths(7); break;
+                case "G": DisplayPaths(8); break;
+                case "N": DisplayPaths(9); break;
+                default:  Console.WriteLine("Nhập sai xin vui lòng nhập lại !"); break;
+            }
+
+        }
+        public void ShowVerts()
+        {
+            foreach (Vertex e in vertexList)
+            {
+                Console.WriteLine((e.label).ToString());
+            }
+        }
+        public void ShowVertex(int v)
+        {
+            Console.WriteLine(vertexList[v].label.ToString() + " ");
+        }
+        private int GetAdjUnvisitedVertex(int v)
+        {
+            for (int j = 0; j <= nVerts - 1; j++)
+                if ((adjMat2[v, j] == 1) && (vertexList[j].isInTree == false))
+                    return j;
+            return -1;
+        }
+        public void BreadthFirstSearch()
+        {
+            Queue gQueue = new Queue();
+            vertexList[0].isInTree = true;
+            ShowVertex(0);
+            gQueue.Enqueue(0);
+            int vert1, vert2;
+            while (gQueue.Count > 0)
+            {
+                vert1 = (int)gQueue.Dequeue();
+                vert2 = GetAdjUnvisitedVertex(vert1);
+                while (vert2 != -1)
+                {
+                    vertexList[vert2].isInTree = true;
+                    ShowVertex(vert2);
+                    gQueue.Enqueue(vert2);
+                    vert2 = GetAdjUnvisitedVertex(vert1);
+                }
+            }
+            for (int i = 0; i <= nVerts - 1; i++)
+                vertexList[i].isInTree = false;
+        }
+        public void DepthFirstSearch()
+        {
+            vertexList[0].isInTree = true;
+            ShowVertex(0);
+            Stack gStack = new Stack();
+            gStack.Push(0);
+            int v;
+            while (gStack.Count > 0)
+            {
+                v = GetAdjUnvisitedVertex((int)gStack.Peek());
+                if (v == -1)
+                    gStack.Pop();
+                else
+                {
+                    vertexList[v].isInTree = true;
+                    ShowVertex(v);
+                    gStack.Push(v);
+                }
+            }
+            for (int j = 0; j <= nVerts - 1; j++)
+                vertexList[j].isInTree = false;
+        }
+    }
+}
---
+++ program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Do_An_Graph
+{
+    class Program
+    {
+    /*    static void Effect(EText[] ET)
+        {
+            Console.CursorVisible = false;
+            string[] str = new string[]
+             {           @" _    _  _____  __    __",
+                       @"| |  | ||  ___||  |  |  |",
+                       @"| |  | || |__  |  |__|  |",
+                       @"| |  | ||  __| |   __   |",
+                       @"\ \__/ /| |___ |  |  |  |",
+                       @" \____/ |_____||__|  |__|" };
+
+
+            int n = str.Length;
+            ET = new EText[n];
+            int w, e,i;
+            w = 15;
+            e = 8;
+            for (i = 0; i < n; i++)
+            {
+                ET[i] = new EText(str[i], w, e + i);
+            }
+            while(i <= 30)
+            {
+                foreach (EText et in ET)
+                {
+                    et.ve();
+                }
+            }
+        }*/
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Graph theGraph = new Graph();
+            theGraph.AddVertex(new Location("1", 10.782967f, 106.6926484f, "Đại Học UEH Cơ Sở A", "Hồ Chí Minh", "3")); //0
+            theGraph.AddVertex(new Location("2", 10.760905f, 106.6657665f, "Đại Học UEH Cơ Sở B", "Hồ Chí Minh", "10")); //1
+            theGraph.AddVertex(new Location("3", 10.773177f, 106.6753573f, "Đại Học UEH Cơ Sở C", "Hồ Chí Minh", "10")); //2
+            theGraph.AddVertex(new Location("4", 10.791699f, 106.6862103f, "Đại Học UEH Cơ Sở D", "Hồ Chí Minh", "1")); //3
+            theGraph.AddVertex(new Location("5", 10.790476f, 106.6971515f, "Đại Học UEH Cơ Sở E", "Hồ Chí Minh", "1")); //4
+            theGraph.AddVertex(new Location("6", 10.760462f, 106.6700192f, "Đại Học UEH Cơ Sở H", "Hồ Chí Minh", "Phú Nhuận")); //5
+            theGraph.AddVertex(new Location("7", 10.782586f, 106.6932257f, "Đại Học UEH Cơ Sở I", "Hồ Chí Minh", "3")); //6
+            theGraph.AddVertex(new Location("8", 10.781823f, 106.6851013f, "Đại Học UEH Cơ Sở V", "Hồ Chí Minh", "3")); //7
+            theGraph.AddVertex(new Location("9", 10.722329f, 106.6194257f, "Đại Học UEH Cơ Sở GDTC", "Hồ Chí Minh", "8")); //8
+            theGraph.AddVertex(new Location("10", 10.705942f, 106.6379664f, "Đại Học UEH Cơ Sở N", "Hồ Chí Minh", "Bình Chánh")); //9
+
+            theGraph.AddEdge(0, 1, 4); theGraph.AddEdge(0, 3, 1);
+            theGraph.AddEdge(1, 2, 2); theGraph.AddEdge(1, 6, 4); theGraph.AddEdge(1, 4, 5);
+            theGraph.AddEdge(2, 6, 3); theGraph.AddEdge(2, 5, 5);
+            theGraph.AddEdge(3, 4, 2); theGraph.AddEdge(3, 7, 2);
+            theGraph.AddEdge(4, 6, 1); theGraph.AddEdge(4, 9, 15);
+            theGraph.AddEdge(6, 8, 12); theGraph.AddEdge(6, 5, 3);
+            theGraph.AddEdge(5, 8, 14); theGraph.AddEdge(5, 9, 17);
+            theGraph.AddEdge(7, 9, 15);
+            theGraph.AddEdge(8, 9, 16);
+
+            
+            // Console.WriteLine("Shortest paths:");
+            // Console.WriteLine();
+
+            // Console.WriteLine();
+            //--------------------------------------------------------------------------------------------------------
+            //    EText[] ET = new EText[30];
+            //    Effect(ET);
+            //    Console.SetWindowSize(100, 40);
+                Console.WriteLine("{0,56}", "ĐỒ ÁN: CẤU TRÚC DỮ LIỆU VÀ GIẢI THUẬT");
+                Console.WriteLine("{0,48}", "CHỦ ĐỀ: ĐỒ THỊ ĐỊA ĐIỂM");
+                Console.WriteLine("{0,46}", "*****************\n");
+                Console.WriteLine("{0,34}\t{1,20}", "DANH SÁCH NHÓM:", "TRẦN HOÀNG THANH THY");
+                Console.WriteLine("{0,58}{1,88}", "NGUYỄN THỊ THẢO LY", "LÂM THỊ YẾN DƯƠNG");
+                Console.WriteLine("{0,58}{1,91}", "NGUYỄN LA HUỆ UYÊN", "PHẠM LÊ PHƯƠNG TRINH");
+                Console.WriteLine(">> Danh sách địa điểm:\n");
+                theGraph.ShowVerts();   Console.WriteLine("------------------------------------------------\n");
+
+                Console.WriteLine("Option 1: Tìm đường đi ngắn nhất");
+                Console.WriteLine("Option 2: Tìm kiếm địa điểm theo thuật toán Depth First Search");
+                Console.WriteLine("Option 3: Tìm kiếm địa điểm theo thuật toán Breadth First Search");
+                Console.WriteLine("Nhập vào option:");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Điểm xuất phát: Đại Học UEH Cơ Sở A");
+                        Console.WriteLine("Nhập vào điểm đến theo ký hiệu tên cơ sở:\nVí dụ: Đại Học UEH Cơ Sở A -> Nhập 'A'");
+                        string cs = Console.ReadLine();
+                        theGraph.DisplayPaths2(cs);
+                        break;
+
+                }
+            Console.ReadLine();
+        }
+    }
+}
